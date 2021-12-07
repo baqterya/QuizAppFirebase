6e0a7779d43438bd77be19c75aff7f2623a993f4
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2021-12-08 00:12:06 +0100

    lost file from previous commit

---
+++ QuizAppFirebase/app/src/main/java/com/example/quizappfirebase/ui/fragment/fragmentutils/AdapterListFavQuestionSets.kt
@@ -0,0 +1,71 @@
+package com.example.quizappfirebase.ui.fragment.fragmentutils
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.quizappfirebase.R
+import com.example.quizappfirebase.data.QuestionSet
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class AdapterListFavQuestionSets(options: FirestoreRecyclerOptions<QuestionSet>)
+    : FirestoreRecyclerAdapter<QuestionSet, AdapterListFavQuestionSets.QuestionSetViewHolder>(options) {
+    private val db = Firebase.firestore
+    private val currentUser = Firebase.auth.currentUser
+
+    class QuestionSetViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuestionSetViewHolder {
+        val objectView = LayoutInflater.from(parent.context).inflate(
+            R.layout.recycler_view_question_set_fav,
+            parent,
+            false
+        )
+        return QuestionSetViewHolder(objectView)
+    }
+
+    override fun onBindViewHolder(
+        holder: QuestionSetViewHolder,
+        position: Int,
+        model: QuestionSet
+    ) {
+        db.collection("users").document(currentUser!!.uid)
+            .get()
+            .addOnSuccessListener { user ->
+                val userFavQuestionSets = user["userFavQuestionSet"] as ArrayList<*>
+
+                val textViewQuestionSetName: TextView = holder.itemView
+                    .findViewById(R.id.text_view_question_set_name_fav)
+                val imageViewQuestionSetMakeFav: ImageView = holder.itemView
+                    .findViewById(R.id.image_view_question_set_make_fav_fav)
+                val textViewQuestionSetAuthorName: TextView = holder.itemView
+                    .findViewById(R.id.text_view_question_set_author_name_fav)
+                val textViewQuestionSetFavCounter: TextView = holder.itemView
+                    .findViewById(R.id.text_view_question_set_fav_counter_fav)
+
+                textViewQuestionSetName.text = model.questionSetName
+                textViewQuestionSetFavCounter.text = model.questionSetFavCount.toString()
+                textViewQuestionSetAuthorName.text = model.questionSetOwnerName
+
+                imageViewQuestionSetMakeFav.setOnClickListener {
+                    val queryQuestionSet = db.collection("questionSets").document(model.questionSetId!!)
+                    val queryUser = db.collection("users").document(currentUser.uid)
+
+                    if (model.questionSetId in userFavQuestionSets) {
+                        queryQuestionSet.update("questionSetFavCount", model.questionSetFavCount - 1)
+                        queryQuestionSet.update("questionSetFavUsersId", FieldValue.arrayRemove(currentUser.uid))
+                        queryUser.update("userFavQuestionSet", FieldValue.arrayRemove(model.questionSetId))
+                    }
+                }
+            }
+    }
+
+
+}
